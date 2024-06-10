062c8fda6bdea08b79bb7e2e38a62271ed341e6d
Vesper Lee <34934765+Vesperlee@users.noreply.github.com>
2024-06-10 14:36:02 +0800

    Update cube.js

--- js/cube.js
+++ js/cube.js
@@ -1,9 +1,4 @@
-import * as THREE from 'three';
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  base: './'
-});
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@v0.165.0/build/three.module.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
